1d9273ac1beb987254cb2d3625637ca8f9bbf1a9
Shubham Vishwakarma <shubhamvishwakarma0604@gmail.com>
2022-09-28 12:57:17 +0530

    Fixed Minor Bugs

--- src/Container/app.js
+++ src/Container/app.js
@@ -1,42 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-    };
-  }
-  componentDidMount() {
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState([""]);
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((usr) => this.setState({ robots: usr }));
-  }
-  Searcher = (evt) => {
-    this.setState({ searchField: evt.target.value.toLowerCase() });
+      .then((usr) => setRobots(usr))
+      .then(() => console.log("done"))
+      .catch((err) => {
+        console.log("Eror Detected, Kindly Check");
+      });
+  }, []);
+  const Searcher = (evt) => {
+    setSearchField(evt.target.value.toLowerCase());
   };
-  render() {
-    const { robots, searchField } = this.state;
-    const FilteredSearch = robots.filter((r) => {
-      return r.name.toLowerCase().includes(searchField);
-    });
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <>
-        <h1 className="tc header"> RoboFriends </h1>
-        <SearchBox Searcher={this.Searcher} />
-        <Scroll>
-          <CardList data={FilteredSearch} />
-        </Scroll>
-        <h1 className="Footer">Made with Love by Shubham Vishwakarma</h1>
-      </>
-    );
-  }
-}
+
+  const FilteredSearch = robots.filter((r) => {
+    return r.name.toLowerCase().includes(searchField);
+  });
+
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <>
+      <h1 className="tc header"> RoboFriends </h1>
+      <SearchBox Searcher={Searcher} />
+      <Scroll>
+        <CardList data={FilteredSearch} />
+      </Scroll>
+      <h1 className="Footer">Made with Love by Shubham Vishwakarma</h1>
+    </>
+  );
+};
 
 export default App;
